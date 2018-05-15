8e738fb9b94c40e364358a11432f4c23da8c838c
Karim Aljandali <karimaljandali@gmail.com>
2018-05-14 23:04:36 -0400

    Updated file to allow navigation from blog landing page

--- Scraper.py
+++ Scraper.py
@@ -2,29 +2,29 @@ import bs4 as bs
 import urllib.request
 import csv
 
-blog_landing = 'http://dmclaw.com/publications'
+blog_landing = 'http://dmclaw.com/publications'   #Add blog landing URL here
 
 sauce = urllib.request.urlopen(blog_landing).read()
 soup = bs.BeautifulSoup(sauce,'lxml')
-
+blog_urls = []
 nav = soup.nav
 
-variable = soup.find("div",{"class":"publications-list"}).findAll("li")
+variable = soup.find("div",{"class":"publications-list"}).findAll("li")  #specify what div each url on the blog landing is under.
 for var in variable:
 	test = var.find("a")
-	print(test)
+	blog_urls.append("http://dmclaw.com/" + test.get("href").replace("%2D","-"))  #change beginning URL 
 
+	print(blog_urls)
 
-# blog_urls = ['http://dmclaw.com/Publication-Susan-H-Briggs-Receives-2017-Altec-All-Star-Award','http://dmclaw.com/Publication-DMC-Expands-Its-Medicare-Compliance-Team']
+# blog_urls = ['http://dmclaw.com/Publication-Susan-H-Briggs-Receives-2017-Altec-All-Star-Award','http://dmclaw.com/Publication-Narrowing-the-Scope-of-Medical-Malpractice-E-Discovery-Requests']  #manual test if necessary
 
-# for url in blog_urls:
-# 	sauce2 = urllib.request.urlopen(url).read()
-# 	soup2 = bs.BeautifulSoup(sauce2,'lxml')
+for url in blog_urls:
+	print("working on" + url) #test to see if loop is working
+	sauce2 = urllib.request.urlopen(url).read()
+	soup2 = bs.BeautifulSoup(sauce2,'lxml')
 
-# 	nav2 = soup2.nav
+	mydiv = soup2.find("div",{"class":"pub-detail"})  #specify what div the blog content is under
 
-# 	mydiv = soup2.findAll("div",{"class":"pub-detail"})
-
-# 	with open('index.csv','a') as csv_file:
-# 		writer = csv.writer(csv_file)
-# 		writer.writerow([mydiv])
+	with open('index.csv','a') as csv_file: #rename csv file to website name.csv
+		writer = csv.writer(csv_file)
+		writer.writerow([mydiv])
