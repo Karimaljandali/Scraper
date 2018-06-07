9920f2cf910e80f55cbb7a32e4b4a7a8539595a2
Karim Aljandali <karimaljandali@gmail.com>
2018-06-07 00:17:55 -0400

    Added title and body section. Also scraper will now delete csv file at each run so there  is no duplicate data if ran twice.

--- Scraper.py
+++ Scraper.py
@@ -9,6 +9,12 @@
 import bs4 as bs
 import urllib.request
 import csv
+import contextlib
+import os
+
+with contextlib.suppress(FileNotFoundError): #Delete index.csv if it exists already, don't want duplicate data if we have to run again.
+    os.remove("index.csv")
+
 
 blog_landing = 'http://dmclaw.com/publications'   #Add blog landing URL here
 
@@ -24,7 +30,12 @@ for var in variable:
 
 	print(blog_urls)
 
-# blog_urls = ['http://dmclaw.com/Publication-Susan-H-Briggs-Receives-2017-Altec-All-Star-Award','http://dmclaw.com/Publication-Narrowing-the-Scope-of-Medical-Malpractice-E-Discovery-Requests']  #manual test if necessary
+# blog_urls = ['http://dmclaw.com/Publication-Susan-H-Briggs-Receives-2017-Altec-All-Star-Award',
+# 'http://dmclaw.com/Publication-Narrowing-the-Scope-of-Medical-Malpractice-E-Discovery-Requests']  #manual test if necessary
+
+with open('index.csv','a') as csv_file: #rename csv file to website name.csv
+		writer = csv.writer(csv_file)
+		writer.writerow(["Site", "HTML", "title", "body"]) #Write header
 
 for url in blog_urls:
 	print("working on" + url) #test to see if loop is working
@@ -32,7 +43,9 @@ for url in blog_urls:
 	soup2 = bs.BeautifulSoup(sauce2,'lxml')
 
 	mydiv = soup2.find("div",{"class":"pub-detail"})  #specify what div the blog content is under
+	title = mydiv.h1
+	body = mydiv.findAll("p")
 
 	with open('index.csv','a') as csv_file: #rename csv file to website name.csv
 		writer = csv.writer(csv_file)
-		writer.writerow([mydiv])
+		writer.writerow([url,mydiv,title,body])
