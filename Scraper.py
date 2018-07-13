fc50f53d58b5edeccd0d5e648573a4ea62e22364
Karim Aljandali <karimaljandali@gmail.com>
2018-07-12 23:22:12 -0400

    Fixed issue with blank rows and added some error handling

--- Scraper.py
+++ Scraper.py
@@ -5,29 +5,28 @@
 
 #Directions:  Install the newest version of python.  Then pip install beautifulsoup4 and pip install lxml
 
-
 import bs4 as bs
 import urllib.request
 import csv
 import contextlib
 import os
-import re
 
 with contextlib.suppress(FileNotFoundError): #Delete index.csv if it exists already, don't want duplicate data if we have to run again.
     os.remove("index.csv")
 
 
-blog_landing = 'https://www.paperstreet.com/blog/category/design/'   #Add blog landing URL here
+blog_landing = 'http://defensecounsel.com/news/press-releases/'   #Add blog landing URL here
 
 sauce = urllib.request.urlopen(blog_landing).read()
-soup = bs.BeautifulSoup(sauce,'lxml')
+soup = bs.BeautifulSoup(sauce)
 blog_urls = []
 nav = soup.nav
 
-variable = soup.find("div",{"class":"content-section"}).findAll("div",{"class":"post-excerpt"})  #specify what div each url on the blog landing is under.
+variable = soup.find("div",{"class":"column-right"}).findAll("h3",{"class":"ccm-page-list-title"})  #specify what div each url on the blog landing is under.
 for var in variable:
-	test = var.find("a",{"class":"learn-more-green"}).get("href")
-	blog_urls.append(test)  #Add each link to blog to list.
+	test = var.find("a").get("href")
+	url = 'http://defensecounsel.com/news/press-releases/' + test #Convert relative link to absolute
+	blog_urls.append(url)  #Add each link to blog to list.
 	print(blog_urls)
 
 	##################################################
@@ -36,23 +35,23 @@ for var in variable:
 	#    content from the actual page itself.        #
 	##################################################
 
-# blog_urls = ['http://dmclaw.com/Publication-Susan-H-Briggs-Receives-2017-Altec-All-Star-Award',
-# 'http://dmclaw.com/Publication-Narrowing-the-Scope-of-Medical-Malpractice-E-Discovery-Requests']  #manual test if necessary
+# blog_urls = ['https://www.paperstreet.com/blog/law-firm-content-inspiration-best-law-firm-content-of-2018/']  #manual test if necessary
 
-with open('index.csv','a') as csv_file: #rename csv file to website name.csv
+with open('index.csv','w', newline='', encoding="utf-8") as csv_file: #rename csv file to website name.csv
 		writer = csv.writer(csv_file)
-		writer.writerow(["Site", "HTML", "title", "body"]) #Write header
+		writer.writerow(["Site", "Raw HTML", "Title", "Body"]) #Write header
 
 for url in blog_urls:
-	print("working on" + url) #test to see if loop is working
+	print("working on " + url) #test to see if loop is working
 	sauce2 = urllib.request.urlopen(url).read()
-	soup2 = bs.BeautifulSoup(sauce2,'lxml')
+	soup2 = bs.BeautifulSoup(sauce2)
 
-	mydiv = soup2.find("div",{"class":"entry-content"})  #specify what div the blog content is under
-	title = mydiv.h1
-	# re.sub('<[^>]+>', '', title) #Strip all content between <> for title. Only text is left no HTML tags.
+	mydiv = soup2.find("div",{"class":"column-right"})  #specify what div the blog content is under
+	try:
+		title = mydiv.h1.text
+	except AttributeError:
+		title = "There was no H1 on this page!"
 	body = mydiv.findAll("p")
-
-	with open('index.csv','a') as csv_file: #rename csv file to website name.csv
+	with open('index.csv','a', newline='', encoding="utf-8") as csv_file: #rename csv file to website name.csv
 		writer = csv.writer(csv_file)
-		writer.writerow([url,mydiv,title,body])
+		writer.writerow([url, mydiv, title, body])
